58bd64d36e49ea3dfea2ccf3ca2e4ac29552b95a
Ashish oli <oliashish3@gmail.com>
2021-02-22 07:48:32 +0530

    commit before deploying

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.use("/", routes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("/client/build"));
 }
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(PORT, () => {
