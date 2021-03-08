089cb3235513e97d6a297b51d24e6e98f3fcc57a
Ashish oli <oliashish3@gmail.com>
2021-03-08 23:37:28 +0530

    footer modified with new UI. Added message from university paragraph, help&queries form and contact deatails section.

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -6,56 +6,93 @@ const Links = ["link1", "link2", "link3", "link4"];
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-center text-light mt-5">
+        <footer className="bg-dark text-center text-light mt-5 pt-3 pb-3">
             <div className="container p-4 pt-5">
                 <div className="row">
-                    <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                        <h5 className="text-uppercase">Footer Content</h5>
+                    <div className="col-lg-4 border-lining-right col-md-8 mb-4 mb-md-0">
+                        <h5 className="footer-text-styling">
+                            G.B.Pant Library,Nainital
+                            <span className="under-line-card mt-3"></span>
+                        </h5>
 
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Iste atque ea quis molestias. Fugiat pariatur
-                            maxime quis culpa corporis vitae repudiandae aliquam
-                            voluptatem veniam, est atque cumque eum delectus
-                            sint!
+                        <p className="pt-2">
+                            We welcome you to our prestigious organisation help
+                            you build a great exprience togather. We here work
+                            as a team and help each other grow. We are team of
+                            highly qualified professionals who are always there
+                            to help you at any time. And we suppose you to help
+                            us with the suggestions so that we too can improve
+                            and serve you the best
                         </p>
+                        <input type="submit" value="We Welcome You"></input>
                     </div>
 
-                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 className="text-uppercase">Links</h5>
-
-                        <ul className="list-unstyled mb-0">
-                            {Links.map((link) => {
-                                return (
-                                    <li>
-                                        <a href="#!" className="text-light">
-                                            {link}
-                                        </a>
-                                    </li>
-                                );
-                            })}
-                        </ul>
+                    <div className="col-lg-4 border-lining-right col-md-6 mb-4 mb-md-0">
+                        <h5 className="footer-text-styling">Help & Queries</h5>
+                        <span className="under-line-card mt-3"></span>
+                        <input
+                            className="mt-3"
+                            type="text"
+                            id="name"
+                            name="studentname"
+                            placeholder="Your name..."
+                        ></input>
+                        <input
+                            type="text"
+                            id="email"
+                            name="studentemail"
+                            placeholder="Your e-mail..."
+                        ></input>
+                        <textarea
+                            id="subject"
+                            name="subject"
+                            placeholder="Help & Queries..."
+                        ></textarea>
+                        <input type="submit" value="Submit"></input>
                     </div>
 
-                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 className="text-uppercase mb-0">Links</h5>
-
-                        <ul className="list-unstyled">
-                            {Links.map((link) => {
-                                return (
-                                    <li>
-                                        <a href="#!" className="text-light">
-                                            {link}
-                                        </a>
-                                    </li>
-                                );
-                            })}
-                        </ul>
+                    <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                        <h5 className="footer-text-styling">Contact Us</h5>
+                        <span className="under-line-card mt-3"></span>
+                        <div className="container-fluid d-flex flex-column mt-3">
+                            <div className="w-100 mx-auto text-start">
+                                <p className="text-alignment ">
+                                    <i className="fa fa-map-marker fa-2x mx-3"></i>
+                                    D.S.B. Campus,Ayarpata,
+                                    <br />
+                                    Nainital(263001),Uttarakhand
+                                </p>
+                            </div>
+                            <div className="mt-3 w-100 mx-auto text-start">
+                                <p className="text-alignment ">
+                                    <i className="fa fa-envelope fa-2x mx-3"></i>
+                                    registrar@kunainital.ac.in
+                                </p>
+                            </div>
+                            <div className="mt-3 w-100 mx-auto text-start">
+                                <p className="text-alignment ">
+                                    <i className="fa fa-phone fa-2x mx-3"></i>
+                                    (05942)- 12232, 42838, 32423
+                                </p>
+                            </div>
+                            <div className="mt-3 w-100 mx-auto text-start">
+                                <p className="text-alignment ">
+                                    <i className="fa fa-phone fa-2x mx-3"></i>
+                                    +91-9870688698
+                                </p>
+                            </div>
+                            <div className="mt-3 w-100 mx-auto text-start">
+                                <p className="text-alignment ">
+                                    <i className="fa fa-internet-explorer fa-2x mx-3"></i>
+                                    www.libraryku.herokuapp.com
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div className="text-center p-3">
+            <div className="text-center footer-text-styling p-3">
                 © 2020 Copyright :{"  "}
                 <a className="text-light" href="/">
                     Library.kumaununiversity.ac.in
